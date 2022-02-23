84fe1895fe04347b5c9c0329de50533a2402d219
ZuJa <zura.j@thekumba.com>
2022-02-23 17:27:51 +0400

    feat: added constants for routenames

---
+++ src/helpers/constants.js
@@ -0,0 +1,7 @@
+export const routeNames = {
+  CART: "/cart",
+  PRODUCT: "/product/:productId",
+  CATEGORY: "/category/:categoryName?",
+  CATEGORY_ALL: "/category/all",
+  CATEGORY_EXACT: "/category/",
+};
